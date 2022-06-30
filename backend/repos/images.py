3c4627ad5a19978c6d2b5caa4f887fb3a1921d89
ProtKsen <protcenko.kseniya@gmail.com>
2022-06-30 21:39:33 +0500

    feature: add errors handling

--- backend/repos/images.py
+++ backend/repos/images.py
@@ -1,15 +1,21 @@
+from sqlalchemy.exc import IntegrityError
 from backend.db import get_db_session
 from backend.models import Image
+from backend.errors import ConflictError
 
 
 class ImageRepo():
     name = "image"
 
     def add_image(self, name: str, type: int) -> Image:
-        new_image = Image(name=name, type=type)
-        db_session = get_db_session()
-        db_session.add(new_image)
-        db_session.commit()
+        try:
+            new_image = Image(name=name, type=type)
+            db_session = get_db_session()
+            db_session.add(new_image)
+            db_session.commit()
+        except IntegrityError:
+            raise ConflictError(self.name)
+
         return new_image
 
     def get_all(self) -> list[Image]:
