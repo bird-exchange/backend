8a989ed2d885dfa47f39930cd5d0c9828b8a1c26
ProtKsen <protcenko.kseniya@gmail.com>
2022-06-30 21:49:29 +0500

    feature: add errors handling to app

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,25 @@
+from http import HTTPStatus
+
 from flask import Flask
+from pydantic import ValidationError
+from werkzeug.exceptions import HTTPException
 
 from backend.config import get_config
 from backend.db import init_db
+from backend.errors import AppError
 from backend.views import image, task, upload
+
+
+def handle_http_exceptions(error: HTTPException):
+    return {'message': error.description}, error.code
+
+
+def handle_app_error(error: AppError):
+    return {'message': error.reason}, error.status
+
+
+def handle_validation_error(error: ValidationError):
+    return error.json(), HTTPStatus.BAD_REQUEST
 
 
 def create_app(test_config=None):
@@ -23,5 +40,9 @@ def create_app(test_config=None):
     app.register_blueprint(image.view, url_prefix='/api/v1/image')
     app.register_blueprint(task.view, url_prefix='/api/v1/task')
     app.register_blueprint(upload.view, url_prefix='/api/v1/upload')
+
+    app.register_error_handler(HTTPException, handle_http_exceptions)
+    app.register_error_handler(AppError, handle_app_error)
+    app.register_error_handler(ValidationError, handle_validation_error)
 
     return app
