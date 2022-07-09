cbd2ae52608e7d3b1f614bc3d2590e712351819f
ProtKsen <protcenko.kseniya@gmail.com>
2022-07-09 21:36:50 +0500

    tests: add tests for CRUD image

--- tests/test_image.py
+++ tests/test_image.py
@@ -26,13 +26,6 @@ def test_add_image_failed_conflict(client, session):
     assert response.status_code == 409
 
 
-def test_get_all_successed(client, session):
-    ImageFactory.create()
-
-    response = client.get('/api/v1/image/')
-    assert response.status_code == 200
-
-
 def test_add_image_failed_badrequest(client, session):
     image_data = {
         "image_name": "test_image",
@@ -42,4 +35,71 @@ def test_add_image_failed_badrequest(client, session):
         "was_fitted": 0
     }
     response = client.post('/api/v1/image/', json=image_data)
+    assert response.status_code == 400
+
+
+def test_get_all_successed(client, session):
+    ImageFactory.create()
+    response = client.get('/api/v1/image/')
+    assert response.status_code == 200
+
+
+def test_get_by_id_successed(client, session):
+    image = ImageFactory.create()
+    response = client.get(f'api/v1/image/{image.uid}')
+    assert response.status_code == 200
+
+
+def test_get_by_id_failed_notfound(client, session):
+    image = ImageFactory.create()
+    response = client.get(f'api/v1/image/{image.uid + 1}')
+    assert response.status_code == 404
+
+
+def test_delete_all(client, session):
+    for _ in range(10):
+        ImageFactory.create()
+    response = client.delete('api/v1/image/')
+    assert response.status_code == 204
+
+
+def test_delete_by_id_successed(client, session):
+    image = ImageFactory.create()
+    response = client.delete(f'api/v1/image/{image.uid}')
+    assert response.status_code == 204
+
+
+def test_delete_by_id_failed_notfound(client, session):
+    image = ImageFactory.create()
+    response = client.delete(f'api/v1/image/{image.uid + 1}')
+    assert response.status_code == 404
+
+
+def test_update_by_id_image_successed(client, session):
+    image = ImageFactory.create()
+    new_image = {
+        "name": "test_name",
+        "type": 1
+    }
+    response = client.put(f'api/v1/image/{image.uid}', json=new_image)
+    assert response.status_code == 200
+
+
+def test_update_by_id_image_failed_notfound(client, session):
+    image = ImageFactory.create()
+    new_image = {
+        "name": "test_name",
+        "type": 1
+    }
+    response = client.put(f'api/v1/image/{image.uid + 1}', json=new_image)
+    assert response.status_code == 404
+
+
+def test_update_by_id_image_failed_badrequest(client, session):
+    image = ImageFactory.create()
+    new_image = {
+        "image_name": "test_name",
+        "type": 1,
+    }
+    response = client.put(f'api/v1/image/{image.uid}', json=new_image)
     assert response.status_code == 400
