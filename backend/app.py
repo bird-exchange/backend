dd7d6711bcae84849fef33eef8cb8cde7bebd713
ProtKsen <protcenko.kseniya@gmail.com>
2022-07-09 20:43:55 +0500

    feature: change data base session

--- backend/app.py
+++ backend/app.py
@@ -4,8 +4,8 @@ from flask import Flask
 from pydantic import ValidationError
 from werkzeug.exceptions import HTTPException
 
-from backend.config import get_config
-from backend.db import init_db
+from backend.config import config
+from backend.db import create_all, db_session
 from backend.errors import AppError
 from backend.views import image, task, upload
 
@@ -22,11 +22,13 @@ def handle_validation_error(error: ValidationError):
     return error.json(), HTTPStatus.BAD_REQUEST
 
 
-def create_app(test_config=None):
+def shutdown_session(exception=None):
+    db_session.remove()
+
+
+def create_app():
 
     app = Flask(__name__, instance_relative_config=True)
-
-    config = get_config(test_config=test_config)
 
     app.config.from_mapping(
         DATABASE_URL=config.db.url,
@@ -34,15 +36,15 @@ def create_app(test_config=None):
         APP_HOST=config.server.host
     )
 
-    with app.app_context():
-        init_db()
+    create_all()
 
     app.register_blueprint(image.view, url_prefix='/api/v1/image')
     app.register_blueprint(task.view, url_prefix='/api/v1/task')
     app.register_blueprint(upload.view, url_prefix='/api/v1/upload')
-
     app.register_error_handler(HTTPException, handle_http_exceptions)
     app.register_error_handler(AppError, handle_app_error)
     app.register_error_handler(ValidationError, handle_validation_error)
+
+    app.teardown_appcontext(shutdown_session)
 
     return app
--- backend/db.py
+++ backend/db.py
@@ -1,19 +1,21 @@
-from sqlalchemy.ext.declarative import declarative_base
-from flask import current_app
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+from backend.config import config
+
+engine = create_engine(config.db.url)
+
+db_session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
 
-
-def get_db_session():
-    engine = create_engine(current_app.config['DATABASE_URL'])
-    db_session = scoped_session(sessionmaker(bind=engine, expire_on_commit=False))
-    return db_session
+Base.query = db_session.query_property()
 
 
-def init_db():
-    engine = create_engine(current_app.config['DATABASE_URL'])
-    db_session = get_db_session()
-    Base.query = db_session.query_property()
+def create_all():
     Base.metadata.create_all(bind=engine)
+
+
+def drop_all(engine):
+    Base.metadata.drop_all(bind=engine)
