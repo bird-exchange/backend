ad50f1dffbf902ef94619002956326d8feed3c3e
ProtKsen <protcenko.kseniya@gmail.com>
2022-07-09 20:42:09 +0500

    tests: create some fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,44 @@
 import pytest
+
 from backend.app import create_app
+from backend.db import db_session, drop_all, engine
+from tests.factories import ImageFactory
 
 
 @pytest.fixture
 def app():
-    app = create_app(test_config=True)
+    app = create_app()
+
+    app.connection = engine.connect()
+
     yield app
+
+    app.connection.close()
+    drop_all(engine)
+
+
+@pytest.fixture(scope="function")
+def session(app):
+    app.transaction = app.connection.begin()
+    ctx = app.app_context()
+    ctx.push()
+
+    session = db_session()
+
+    ImageFactory._meta.sqlalchemy_session = session
+
+    yield session
+
+    app.transaction.close()
+    session.close()
+    ctx.pop()
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
