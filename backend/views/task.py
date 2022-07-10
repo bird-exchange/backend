3e86667f59947eb648904c264573cf1bb8eeb6ad
ProtKsen <protcenko.kseniya@gmail.com>
2022-07-10 21:05:32 +0500

    feature: create handle for task

--- backend/views/task.py
+++ backend/views/task.py
@@ -1,9 +1,16 @@
+from http import HTTPStatus
+
 from flask import Blueprint
 
+from backend import schemas
+from backend.repos.image import Image
 
 view = Blueprint('task', __name__)
+image_repo = Image()
 
 
 @view.get('/')
 def get_next_task():
-    pass
+    entity = image_repo.get_not_recognized()
+    image = schemas.Image.from_orm(entity)
+    return image.dict(), HTTPStatus.OK
