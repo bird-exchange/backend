6239b22b5e7f857f0bad45216250b2210a50b8a6
ProtKsen <protcenko.kseniya@gmail.com>
2022-07-10 12:07:16 +0500

    feature: create handle upload image

--- backend/views/files.py
+++ backend/views/files.py
@@ -1,11 +1,42 @@
-from flask import Blueprint
+from http import HTTPStatus
+from pathlib import Path
 
-view = Blueprint('upload', __name__)
+from flask import Blueprint, request
+from werkzeug.utils import secure_filename
+
+from backend.aws import s3
+from backend.config import config
+from botocore.exceptions import ClientError
+from backend.errors import RequestNotContainError, AppError, NotAcceptableError
+view = Blueprint('files', __name__)
+
+ALLOWED_EXTENSIONS = set(['.png', '.jpg', '.jpeg'])
 
 
 @view.post('/')
-def download_image():
-    pass
+def upload_image():
+    if not ('file' in request.files):
+        raise RequestNotContainError('file')
+    file = request.files['file']
+    if not file.filename:
+        raise RequestNotContainError('file')
+    filename = secure_filename(file.filename)
+    if not (Path(filename).suffix in ALLOWED_EXTENSIONS):
+        raise NotAcceptableError('file format')
+
+    resp = s3.list_buckets()
+    all_buckets_names = [bucket['Name'] for bucket in resp['Buckets']]
+
+    for bucket_name in [config.aws.bucket_input_images, config.aws.bucket_output_images]:
+        if bucket_name not in all_buckets_names:
+            s3.create_bucket(Bucket=bucket_name)
+
+    try:
+        s3.upload_fileobj(file, config.aws.bucket_input_images, filename)
+    except ClientError:
+        raise AppError("Не удалось сохранить файл", HTTPStatus.NOT_IMPLEMENTED)
+
+    return f'{filename} successfully saved', HTTPStatus.OK
 
 
 @view.get('/<uid>')
