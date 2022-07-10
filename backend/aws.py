d7d36c31880b6ce2ac5e62dd8c7bda6a5276465d
ProtKsen <protcenko.kseniya@gmail.com>
2022-07-10 21:03:49 +0500

    feature: add aws resource

--- backend/aws.py
+++ backend/aws.py
@@ -4,7 +4,14 @@ from backend.config import config
 
 session = boto3.session.Session()
 
-s3 = session.client(
+s3_client = session.client(
+    service_name='s3',
+    endpoint_url='http://127.0.0.1:9000',
+    aws_access_key_id=config.aws.key_id,
+    aws_secret_access_key=config.aws.key,
+)
+
+s3_resource = boto3.resource(
     service_name='s3',
     endpoint_url='http://127.0.0.1:9000',
     aws_access_key_id=config.aws.key_id,
