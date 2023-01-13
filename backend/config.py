9bcb1757bed30d912f4d948cab93c2109f803f12
ProtKsen <protcenko.kseniya@gmail.com>
2023-01-13 18:50:59 +0500

    feature: add aws endpoint

--- backend/config.py
+++ backend/config.py
@@ -17,6 +17,7 @@ class AwsConfig(BaseModel):
     key: str
     bucket_input_images: str
     bucket_output_images: str
+    endpoint: str
 
 
 class AppConfig(BaseModel):
@@ -33,6 +34,7 @@ def load_from_env() -> AppConfig:
     aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
     aws_bucket_input_images = os.environ['AWS_BUCKET_NAME_INPUT_IMAGES']
     aws_bucket_output_images = os.environ['AWS_BUCKET_NAME_OUTPUT_IMAGES']
+    aws_endpoint = os.environ['AWS_ENDPOINT']
     return AppConfig(
         server=Server(port=app_port, host=app_host),
         db=DataBase(url=db_url),
@@ -41,6 +43,7 @@ def load_from_env() -> AppConfig:
             key=aws_secret_access_key,
             bucket_input_images=aws_bucket_input_images,
             bucket_output_images=aws_bucket_output_images,
+            endpoint=aws_endpoint
         )
     )
 
